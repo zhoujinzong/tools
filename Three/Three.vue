7fc15adad598a48bcd518b345d11500eb991560b
zhoujinzong <zhoujinzong@users.noreply.github.com>
2020-11-19 22:51:36 +0800

    Update Three.vue

--- Three/Three.vue
+++ Three/Three.vue
@@ -1,44 +1,46 @@
 <template xmlns:v-bind="http://www.w3.org/1999/xhtml">
 
   <section>
-    <div  id="main_model" class="absoult_all" v-show="isWebGl">
+    <div id="main_model" class="absoult_all" v-show="isWebGl">
       <div id="Stats_output"></div>
-      <div id="circeLeft" v-bind:class="isWebGl ? 'circeLeft':'circeLeft_dis'" @mousedown="circle_action(1, tag_left)"></div>
-      <div id="circeRight" v-bind:class="isWebGl? 'circeRight':'circeRight_dis'" @mousedown="circle_action(-1, tag_left)"></div>
+      <div id="circeLeft" :class="isWebGl ? 'circeLeft':'circeLeft_dis'" @mousedown="circleActionDebounce(1, tag_left)"></div>
+      <div id="circeRight" :class="isWebGl? 'circeRight':'circeRight_dis'"
+           @mousedown="circleActionDebounce(-1, tag_left)"></div>
       <div class="cabinet3D_switch">
         <div v-for="menu in threeD_switch_menu" :id="menu.id"
              v-if="isShowSwitchMenu && menu.isShow" v-on:click="changeView(menu.viewFlag,menu.type)"
              :class="(viewFlag === menu.viewFlag && (menu.type ? current_capacity_type === menu.type : true))? menu.selectClass : ''"
              :title="menu.title"></div>
       </div>
-      <div id="circeReset" title="初始视角" :style="{top: is_qt ? '1rem':'unset'}" @mousedown="circle_action(1, tag_reset)"></div>
+      <div id="circeReset" title="初始视角" :style="{top: is_qt ? '1rem':'unset'}"
+           @mousedown="circleActionDebounce(1, tag_reset)"></div>
       <div id="CanvasHide" style="display: none"></div>
-      <div class="loading_center" v-show="isLoading"></div>
+<!--      <div class="loading_center" v-show="isLoading"></div>-->
       <div class="bangben_warning" v-show="cubeArry.length===0 &&!isLoading">暂无数据，请添加机柜</div>
-      <div id="color_temp" class="color_list" v-show="viewFlag === 1" >
+      <div id="color_temp" class="color_list" v-show="viewFlag === 1">
         <div id="temp_text" class="color_top_text">温度（℃）</div>
         <span id="temp_16"><16</span>
         <span id="temp_22">22</span>
         <span id="temp_28">28</span>
         <span id="temp_34">34></span>
       </div>
-        <!--容量温度柱图参考开始-->
-      <div id="color_cap" class="color_list" v-show="viewFlag === 2 || viewFlag === 6" >
+      <!--容量温度柱图参考开始-->
+      <div id="color_cap" class="color_list" v-show="viewFlag === 2 || viewFlag === 6">
         <div id="cap_text" class="color_top_text" v-text="capTypeText"></div>
         <div id="cap_num"></div>
       </div>
-<!--        容量温度柱图参考结束-->
+      <!--        容量温度柱图参考结束-->
       <!--旧的云图切换开始-->
-<!--      <label v-if="viewFlag === 1" class="heatmap_type" :style="{right: heatmap_type === 1 ? (LCD === 1 ? '4.5rem':'3.5rem'):'1rem'}">-->
-<!--        <select v-model.number="heatmap_type" @change="change_heatmap_view1">-->
-<!--          <option v-for="(val,key) in three_map_type" :value="key">{{val}}</option>-->
-<!--        </select>-->
-<!--      </label>-->
-<!--      <label v-if="viewFlag === 1" class="heatmap_type_1">-->
-<!--        <select v-if="heatmap_type === 1 || true" v-model.number="heatmap_view" @change="change_heatmap_view">-->
-<!--          <option v-for="(val,key) in three_map_chose" :value="key">{{val}}</option>-->
-<!--        </select>-->
-<!--      </label>-->
+      <!--      <label v-if="viewFlag === 1" class="heatmap_type" :style="{right: heatmap_type === 1 ? (LCD === 1 ? '4.5rem':'3.5rem'):'1rem'}">-->
+      <!--        <select v-model.number="heatmap_type" @change="change_heatmap_view1">-->
+      <!--          <option v-for="(val,key) in three_map_type" :value="key">{{val}}</option>-->
+      <!--        </select>-->
+      <!--      </label>-->
+      <!--      <label v-if="viewFlag === 1" class="heatmap_type_1">-->
+      <!--        <select v-if="heatmap_type === 1 || true" v-model.number="heatmap_view" @change="change_heatmap_view">-->
+      <!--          <option v-for="(val,key) in three_map_chose" :value="key">{{val}}</option>-->
+      <!--        </select>-->
+      <!--      </label>-->
       <!--旧的云图切换结束-->
     </div>
 
@@ -49,10 +51,12 @@
     </div> -->
 
     <!--机柜浮动信息 -->
-    <div class="main_ico_3d" id="main_ico_3d" v-show="devShow" v-html="nowItme.name" v-bind:style="{top:nowItme.y + 'px',left:nowItme.x + 'px',color: '#ffffff'}"></div>
-<!--    <div v-show="viewFlag === 1 || viewFlag === 6" id="threeD_chose_menu_center" class="position_center" @click="show_threeD_chose_menu"></div>-->
-    <div class="main_ico_3d threeD_chose_menu_3d" id="main_3d_menu" v-show="threeD_chose_menu_show" v-clickOutSide="show_false"
-         v-bind:style="{top:threeD_chose_menu_position.y + 'px',left:threeD_chose_menu_position.x + 'px',color: '#ffffff'}">
+    <div class="main_ico_3d" id="main_ico_3d" v-show="devShow" v-html="nowItme.name"
+         :style="{top:nowItme.y + 'px',left:nowItme.x + 'px',color: '#ffffff'}"></div>
+    <!--    <div v-show="viewFlag === 1 || viewFlag === 6" id="threeD_chose_menu_center" class="position_center" @click="show_threeD_chose_menu"></div>-->
+    <div class="main_ico_3d threeD_chose_menu_3d" id="main_3d_menu" v-show="threeD_chose_menu_show"
+         v-clickOutSide="show_false"
+         :style="{top:threeD_chose_menu_position.y + 'px',left:threeD_chose_menu_position.x + 'px',color: '#ffffff'}">
       <div id="left_sanjiao"></div>
       <ul class="threeD_chose_menu">
         <li v-for="(menu,index) in threeD_chose_menu"
@@ -62,75 +66,74 @@
           <div class="threeD_chose_menu_div">
             <span class="menu_img"></span>
             <span v-text="menu.name"></span>
-            <span v-show="menu.children" :class="['threeD_chose_menu_arrow',menu.showChildren ? 'threeD_chose_menu_arrow_down':'threeD_chose_menu_arrow_up']"></span>
+            <span v-show="menu.children"
+                  :class="['threeD_chose_menu_arrow',menu.showChildren ? 'threeD_chose_menu_arrow_down':'threeD_chose_menu_arrow_up']"></span>
           </div>
           <ul v-show="menu.showChildren">
             <li v-for="children in menu.children" @click="change_heatmap_view($event,children.value,children.key)">
               <div>
-                <span v-text="children.name" :class="threeD_chose_menu_chose_judge(children.key,children.value) && 'threeD_chose_menu_chose'" ></span>
+                <span v-text="children.name"
+                      :class="threeD_chose_menu_chose_judge(children.key,children.value) && 'threeD_chose_menu_chose'"></span>
               </div>
             </li>
           </ul>
         </li>
       </ul>
     </div>
-    <mainPopWin idn="main_three_pop" v-on:showChange="showChange" v-bind="{showFlag:showFlag,box_index:box_index,cab_type:0,dev_index:0}"></mainPopWin>
+    <mainPopWin idn="main_three_pop" v-on:showChange="showChange"
+                v-bind="{showFlag:showFlag,box_index:box_index,cab_type:0,dev_index:0}"></mainPopWin>
     <!--<mobileSoftKey v-if="LCD"></mobileSoftKey>-->
 
-    <popWindow idn="main_cabinet_message" buttonNum="0" :title="cabinet_pop_title + textDetail" v-bind:close1-click="main_normal_close">
-      <ul class="main_cabinet_pop" v-bind:class="(pc_data.length===0)?'main_cabinet_popS':''">
-
-        <li class="main_cabinet_li">
-          <div class="mCabmin_ul">
-            <p class="mCabmin_ul_thead">温湿度</p>
-            <div class="mCabmin_ul_pd" v-for="(el,kl) in cold_hot">
-              <table class="main_table">
-                <thead>
-                <tr>
-                  <td colspan="3" v-bind:class="(kl=='cold')?'mC_yel_bg':'mC_green_bg'">
-                    {{(kl=='cold')? textCold : textHot}}
-                  </td>
-                </tr>
-                </thead>
-                <tbody>
-                <tr><td>{{AveEnable==1 ? '': textDevType}}</td><td><span>温度</span>(℃)</td><td><span>湿度</span>(%RH)</td></tr>
-                <tr v-if="el.length===0"><td colspan="3" style="text-align:center">暂无数据</td></tr>
-                <tr v-for="(vv,kk) in el">
-                  <td v-bind:title="vv.name_f">{{vv.name_f}}</td>
-                  <td>{{vv.Tem}}</td>
-                  <td>{{vv.Humi}}</td>
-                </tr>
-                </tbody>
-              </table>
+    <popWindow idn="main_cabinet_message" buttonNum="0" title="机柜设备信息" width="40"
+               :close1-click="main_normal_close" :close-click0="main_normal_close">
+      <div class="flex-c-c">
+        <div :class="['main_cabinet_pop_left',cabinetTypeImg]"></div>
+        <ul class="main_cabinet_pop">
+          <li class="main_cabinet_li" v-for="devData in popCabinetDevData">
+            <div class="mCabmin_ul">
+              <div class="mCabmin_ul_pd mythead">
+                <table class="main_table">
+                  <thead>
+                  <tr>
+                    <td
+                      v-for="(val,key) in devData.title"
+                      :title="val"
+                      v-html="val"
+                      v-if="val"
+                      :width="widthLimit(key)"
+                    ></td>
+                  </tr>
+                  </thead>
+                </table>
+              </div>
+              <div class="mCabmin_ul_pd mytbody">
+                <table class="main_table">
+                  <tbody>
+                  <tr v-if="devData.value.length === 0">
+                    <td colspan="3" style="text-align:center">暂无数据</td>
+                  </tr>
+                  <tr v-for="(vl,kl) in devData.value">
+                    <td
+                      v-for="(v,k) in vl"
+                      :title="devData.tips[kl] && devData.tips[kl][k]"
+                      v-html="v"
+                      v-if="v"
+                      :width="widthLimit(k)"
+                    ></td>
+                  </tr>
+                  </tbody>
+                </table>
+              </div>
             </div>
-          </div>
-
-          <div class="mCabmin_ul" v-if="pc_data.length!==0">
-            <p class="mCabmin_ul_thead">配电</p>
-            <table  class="main_table">
-              <tbody>
-              <tr>
-                <td>
-                  <ul class="mCabmin_ul_pd" v-for="(el,kl) in pc_data">
-                    <li class="mCabmin_pd_title" v-bind:title="el.title">{{el.title}}</li>
-                    <li><span>电流</span>(A):{{el.cur}}</li>
-                    <li><span>电压</span>(V):{{el.vol}}</li>
-                    <li><span>功率</span>(kW):{{el.power}}</li>
-                  </ul>
-                </td>
-              </tr>
-              </tbody>
-            </table>
-          </div>
-
-        </li>
-      </ul>
+          </li>
+        </ul>
+      </div>
     </popWindow>
     <popWindow idn="camera_dev_message" buttonNum="0"
                :title="pop_camera_dev.name_f + textDetail">
       <div class="alarm_linkage_table myPoPWin">
         <span>设备状态</span>：<p v-html="deal_dev_status(pop_camera_dev.is_alarm) || textNodata">
-        </p>
+      </p>
       </div>
     </popWindow>
     <popWindow
@@ -146,7 +149,7 @@
 
 </template>
 <style lang="scss" scoped>
-  #test_3d{
+  #test_3d {
     position: absolute;
     bottom: 50%;
     cursor: pointer;
@@ -154,7 +157,8 @@
     right: 1rem;
     top: 2rem;
   }
-  .myPoPWin{
+
+  .myPoPWin {
     width: 19rem;
     min-height: 5rem;
     max-height: 15rem;
@@ -165,8 +169,15 @@
     overflow-x: hidden;
     overflow-y: auto;
   }
-  .my_main_ico_3d{
+
+  .my_main_ico_3d {
     height: 2.4rem;
+  }
+  .mytbody {
+    max-height: 15rem;
+    overflow-x: hidden;
+    overflow-y: auto;
+    /*border-bottom: 0.05rem solid #d2c8b4;*/
   }
 </style>
 
