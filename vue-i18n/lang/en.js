8dd4f3b65866eb608ba9564c62e7484b0e0bbaae
zhoujinzong <zhoujinzong@users.noreply.github.com>
2021-09-24 09:42:05 +0800

    Create en.js

---
+++ vue-i18n/lang/en.js
@@ -0,0 +1,8 @@
+module.exports = {
+	"19a026b2c5ab3059747e8d5636446978": "No alarm data",
+	"8fc99b5d59c91b2384e50bf17677beb3": "Grade rank",
+	"a0dad93d7a3603f6c4383c98490efa82": "Type arrangement",
+	"bcc241e075739a3a3a815151ce33fd0b": "No equipment",
+	"625fb26b4b3340f7872b411f401e754c": "Cancel",
+	"38cf16f2204ffab8a6e0187070558721": "Determine"
+}
