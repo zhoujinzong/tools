a85cb9cd9a0a03d8862d19a2d7d45d8a6110e0e4
zhoujinzong <zhoujinzong@users.noreply.github.com>
2021-09-24 09:35:41 +0800

    Create ignoreFile.js

---
+++ vue-i18n/i18n/ignoreFile.js
@@ -0,0 +1,27 @@
+/*
+ * @Author: your name
+ * @Date: 2020-05-26 09:15:29
+ * @LastEditTime: 2020-10-28 17:56:56
+ * @LastEditors: xyl
+ * @Description: 不提取中文的文件
+ * @FilePath: \bms\src\i18n\ignoreFile.js
+ */
+module.exports = [
+  "./src/i18n/ignore.js",
+  // "./src/libs/public.js",
+  "./src/libs/clickOutSide.js",
+  "./src/libs/qwebchannel.js",
+  "./src/lang/en.js",
+  "./src/lang/zh.js",
+  "./src/lang/zh_cn.js",
+  "./src/axiosConfig/index.js",
+  "./src/plugin/timepicki.js",
+  "./src/router/module/PCRouter.js",
+  "./src/router/module/LCDRouterP.js",
+  "./src/plugin",
+  "./src/plugin/Validform_v5.3.2_min.js",
+  "./src/plugin/cascade/mobiscroll-2.13.2.full.min.js",
+  "./src/plugin/echarts.min.js",
+  "./src/axiosConfig",
+  "./src/i18n",
+];
